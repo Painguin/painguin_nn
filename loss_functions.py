53d158a39555e868cd5a2bba92719238d88b59ab
Pedro Da Cunha <pedrodacunha1217@gmail.com>
2019-05-19 19:38:14 +0200

    abstract the activation function into his own layer

--- loss_functions.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class MSE:
-    def __call__(self, pred, target):
-        return np.sum((pred - target) ** 2)
-    
-    def derivative(self, pred, target):
-        return -2 * (target - pred)
--- nn_lib.py
+++ nn_lib.py
@@ -1,75 +1,113 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, from_, to, activation):
-        self.W = np.random.normal(scale=np.sqrt(2 / (from_ + to)), size=(from_, to))
-        self.b = np.zeros(to)
-        self.activation = activation
-        
-    def __call__(self, X):
-        return self.activation(X @ self.W + self.b)
-    
     def forward(self, x):
-        s = x @ self.W + self.b
-        y = self.activation(s)
-        return y, s
+        return NotImplementedError()
     
-    def backward(self, x, s, dl_dx):
-        dl_ds = dl_dx * self.activation.derivative(s)
-        dl_dw = x.T @ dl_ds
-        dl_db = dl_ds.sum(axis=0)
-        dl_dx_prev = dl_ds @ self.W.T
-        
-        return dl_dw, dl_db, dl_dx_prev
+    def backward(self, x):
+        return NotImplementedError()
 
-class NeuralNetwork:
-    def __init__(self, layers, loss):
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def params(self):
+        return []
+
+
+class Sequential(Layer):
+    def __init__(self, *layers):
         self.layers = layers
-        self.L = len(layers)
-        self.loss = loss
-        
-    def __call__(self, X):
-        x = X
-        for l in range(self.L):
-            x = self.layers[l](x)
-        return x
-    
-    def forward(self, x):
-        x_arr = [x]
-        s_arr = []
-        
-        for layer in self.layers:
-            x_lay, s_lay = layer.forward(x_arr[-1])
-            x_arr.append(x_lay)
-            s_arr.append(s_lay)
-            
-        return x_arr, s_arr
-    
-    def backward(self, x_arr, s_arr, dl_dx):
-        dl_dw = []
-        dl_db = []
-        
-        for layer, x, s in zip(reversed(self.layers), reversed(x_arr), reversed(s_arr)):
-            dl_dw_l, dl_db_l, dl_dx = layer.backward(x, s, dl_dx)
-            dl_dw.insert(0, dl_dw_l)
-            dl_db.insert(0, dl_db_l)
-            
-        return dl_dw, dl_db
 
-    def backpropagate(self, x, y):
-        # forward pass
-        x_arr, s_arr = self.forward(x)
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, dl_dy):
+        for layer in reversed(self.layers):
+            dl_dy = layer.backward(dl_dy)
+        return dl_dy
+
+    def params(self):
+        return [param for layer in self.layers for param in layer.params()]
+
+
+class Linear(Layer):
+    def __init__(self, from_, to):
+        self.w = np.random.normal(scale=np.sqrt(2 / (from_ + to)), size=(from_, to))
+        self.b = np.zeros(to)
+        self.dl_dw = np.zeros_like(self.w)
+        self.dl_db = np.zeros_like(self.b)
         
-        # backward pass
-        dl_dx = self.loss.derivative(x_arr.pop(), y)
-        dl_dw, dl_db = self.backward(x_arr, s_arr, dl_dx)
-        
-        return dl_dw, dl_db
+    def forward(self, x):
+        self.input = x
+        return x @ self.w + self.b
     
-    def fit(self, X, y, epochs=10, batch_size=128, gamma=0.01):
+    def backward(self, dl_dy):
+        self.dl_dw += self.input.T @ dl_dy
+        self.dl_db += dl_dy.sum(axis=0)
+        return dl_dy @ self.w.T
+    
+    def params(self):
+        return [(self.w, self.dl_dw), (self.b, self.dl_db)]
+    
+class ReLU(Layer):
+    def forward(self, x):
+        self.input = x
+        return (x > 0) * x
+
+    def backward(self, dl_dy):
+        return np.sign(self.input) * dl_dy
+
+class Tanh(Layer):
+    def forward(self, x):
+        self.input = x
+        exp_ = np.exp(2 * x)
+        return (exp_ - 1) / (exp_ + 1)
+
+    def backward(self, dl_dy):
+        exp_ = np.exp(2 * self.input)
+        return dl_dy * 4 * exp_ / (exp_ + 1) ** 2
+
+class Loss:
+    def forward(self, pred, target):
+        return NotImplementedError()
+
+    def backward(self, pred, target):
+        return NotImplementedError()
+    
+    def __call__(self, pred, target):
+        return self.forward(pred, target)
+
+class MSE(Loss):
+    def forward(self, pred, target):
+        return np.sum(
+            (target - pred) ** 2
+        )
+
+    def backward(self, pred, target):
+        return -2 * (target - pred)
+
+class SGD:
+    def __init__(self, params, gamma):
+        self.params = params
+        self.gamma = gamma
+
+    def update(self):
+        for param, dl_param in self.params:
+            param -= self.gamma * dl_param
+            dl_param.fill(0)
+
+class Model:
+    def __init__(self, model, loss, optimizer):
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+    
+    def fit(self, x, y, epochs=10, batch_size=128):
         for _ in range(epochs):
-            for b in range(0, X.shape[0], batch_size):
-                dl_dw, dl_db = self.backpropagate(X[b: b + batch_size], y[b: b + batch_size])
-                for layer, dl_dw_l, dl_db_l in zip(self.layers, dl_dw, dl_db):
-                    layer.W -= gamma * dl_dw_l
-                    layer.b -= gamma * dl_db_l
+            for b in range(0, x.shape[0], batch_size):
+                output = self.model(x[b: b + batch_size])
+                dl_dy = self.loss.backward(output, y[b: b + batch_size])
+                self.model.backward(dl_dy)
+                self.optimizer.update()
